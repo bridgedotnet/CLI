5c1ed7a5548276a07bcc63920ed0acb38d315f69
Fabrício Zimmerer Murta <fabricio@object.net>
2018-02-05 14:44:29 -0200

    Updates mkzip script file naming convention.
    Uses same convention from installer and dmg packages.
    Also unifies the zip file name reference to a single and only variable.

--- installer/windows/mkzip.sh
+++ installer/windows/mkzip.sh
@@ -3,6 +3,8 @@
 # Zipfile building script, currently requires cygwin/*nix due to the need to
 # have a 'zip' command on path.
 
+pkgname_prefix="bridge-cli."
+
 if [ ! -d Bridge/bin/Release ]; then
  echo "This should be run from the repo's root."
  exit 1
@@ -17,16 +19,16 @@ ver="$(egrep "^\[assembly: *AssemblyInformationalVersion\(" \
  Bridge/Properties/AssemblyInfo.cs | cut -f2 -d\")"
 
 if [ -z "${ver}" ]; then
- echo "Unable to infer bridge-cli version."
+ echo "Unable to infer bridge CLI version."
  exit 1
 fi
 
-mkdir bridge-cli-${ver}
+mkdir "${pkgname_prefix}${ver}"
 cp -pr LICENSE Bridge/bin/Release/{bridge.exe,templates,tools} \
- bridge-cli-${ver}/.
-cp installer/windows/zipfile-readme.txt bridge-cli-${ver}/readme.txt
-zip -r bridge-cli-${ver}.zip bridge-cli-${ver}
+ "${pkgname_prefix}${ver}"/.
+cp installer/windows/zipfile-readme.txt "${pkgname_prefix}${ver}"/readme.txt
+zip -r "${pkgname_prefix}${ver}.zip" "${pkgname_prefix}${ver}"
 
-rm -rf bridge-cli-${ver}
+rm -rf "${pkgname_prefix}${ver}"
 
-echo "Package created at: bridge-cli-${ver}.zip"
+echo "Package created at: ${pkgname_prefix}${ver}.zip"
