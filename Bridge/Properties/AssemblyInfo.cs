1969cf40180bd1d0eb223b0592c0abfe67dd82b0
Fabrício Zimmerer Murta <fabricio@object.net>
2018-02-16 20:52:18 -0200

    Updates version and Bridge references to 16.8.0.

--- Bridge/Properties/AssemblyInfo.cs
+++ Bridge/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("16.7.0.0")]
-[assembly: AssemblyFileVersion("16.7.2")]
-[assembly: AssemblyInformationalVersion("16.7.2")]
+[assembly: AssemblyVersion("16.8.0.0")]
+[assembly: AssemblyFileVersion("16.8.0")]
+[assembly: AssemblyInformationalVersion("16.8.0")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
 [assembly: AssemblyCopyright("Copyright 2008-2018 Object.NET, Inc.")]
 [assembly: AssemblyTitle("Bridge CLI")]
