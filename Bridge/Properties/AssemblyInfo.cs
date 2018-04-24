4257d05ba181cdf9d49bb6b70830f0453c908638
Fabrício Zimmerer Murta <fabricio@object.net>
2018-04-23 22:37:52 -0300

    Updates version and references to Bridge to 17.1.0.

--- Bridge/Properties/AssemblyInfo.cs
+++ Bridge/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("17.0.0.0")]
-[assembly: AssemblyFileVersion("17.0.0.0")]
-[assembly: AssemblyInformationalVersion("17.0.0")]
+[assembly: AssemblyVersion("17.1.0.0")]
+[assembly: AssemblyFileVersion("17.1.0.0")]
+[assembly: AssemblyInformationalVersion("17.1.0")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
 [assembly: AssemblyCopyright("Copyright 2008-2018 Object.NET, Inc.")]
 [assembly: AssemblyTitle("Bridge CLI")]
